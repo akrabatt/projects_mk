40639bbf0ac88729c91f93b3dc140deb38281a24
Dmitrii_rz <starwars-1999@yandex.ru>
2024-04-11 10:17:29 +0200

    deleted SPI

--- main.c
+++ main.c
@@ -49,23 +49,15 @@ int main(void)
 //    usart4.mb_status.tx_mode = INT_type;
     
 //    load_config ();
-//    IC1CONbits.ON = 1; 
-//    IC6CONbits.ON = 1; 
-    IC3CONbits.ON = 1; 
-    IC4CONbits.ON = 1; 
-    IC7CONbits.ON = 1; 
-    IC8CONbits.ON = 1; 
-//    load_config ();   
     while(1)
 
     {
-    mbs (&usart5, 1);				// порт  1
+    mbs (&usart5, 1);				// пїЅпїЅпїЅпїЅ  1
     stop_uart_tx_dma();   
     mbs (&usart4, 1);				//  4
 //    stop_uart_tx();
     PORTGbits.RG7 = help_strobe & help_reset;
     if (start_ctrl == 1)   { start_ctrl = 0;   main_control ();   }
-    if (start_pid_reg == 1) {start_pid_reg = 0; pid_control(MB_conf.CV_KP, MB_conf.CV_KD, MB_conf.CV_KI);}
     
     LED_8 = help_strobe & help_reset;
     }
